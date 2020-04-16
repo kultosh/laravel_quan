80e057c78ca8a3c429b76681db62caeb07e6b32a
kultosh <kulgole90@gmail.com>
2020-04-16 17:39:17 +0545

    Answer Scheme is designed for the questions

---
+++ app/Answer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    public function question()
+    {
+    	return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+}
