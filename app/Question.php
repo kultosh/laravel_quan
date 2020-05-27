170be43ca5035f7dd3aff88d582c3ece39179469
kultosh <kulgole90@gmail.com>
2020-05-27 15:13:22 +0545

    Securing the application from xss attack

--- app/Question.php
+++ app/Question.php
@@ -20,6 +20,11 @@ class Question extends Model
         $this->attributes['slug'] = str_slug($value);
     }
 
+    // public function setBodyAttribute($value)
+    // {
+    //     $this->attributes['body'] = clean($value);
+    // }
+
     public function getUrlAttribute()
     {
     	return route("questions.show", $this->slug);
@@ -45,7 +50,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers()
@@ -77,5 +82,19 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites()->count();
+    }
+
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {
+        return str_limit(strip_tags($this->bodyHtml()), $length);
+    }
+
+    private function bodyHtml(){
+        return \Parsedown::instance()->text($this->body);
     }
 }
