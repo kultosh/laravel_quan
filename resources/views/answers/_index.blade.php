4faaf05c6e91fe1aa8cae65cd156d1a2f818ac9f
kultosh <kulgole90@gmail.com>
2020-05-31 14:18:36 +0545

    Tidy up the view

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -11,55 +11,7 @@
                     @include('layouts._messages')
 
                     @foreach ($answers as $answer)
-                        <div class="media">
-                            <div class="d-fex flex-column vote-controls">
-                                <a title="This answer is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}" onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{$answer->id}}').submit();">
-                                    <i class="fas fa-caret-up fa-3x"></i>
-                                </a>
-                                <form id="up-vote-answer-{{$answer->id}}" action="/answers/{{$answer->id}}/vote" method="post">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="1">
-                                </form>
-
-                                <span class="votes-count">{{$answer->votes_count}}</span>
-                                <a title="This answer is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}" onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{$answer->id}}').submit();">
-                                    <i class="fas fa-caret-down fa-3x"></i>
-                                </a>
-                                <form id="down-vote-answer-{{$answer->id}}" action="/answers/{{$answer->id}}/vote" method="post">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="-1">
-                                </form>
-                            
-                            </div>
-                            <div class="media-body">
-                                {!! $answer->body_html !!}
-                                <div class="row">
-                                    <div class="col-4">
-                                        <div class="ml-auto">
-                                            @can('update', $answer)
-                                            <a href="{{route('questions.answers.edit', [$question->id,$answer->id])}}" class="btn btn-outline-info btn-sm">Edit</a>
-                                            @endcan
-
-                                            @can('delete', $answer)
-                                            <form action="{{route('questions.answers.destroy', [$question->id,$answer->id])}}" method="post" class="form-delete">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                            </form>
-                                            @endcan
-                                        </div>
-                                    </div>
-                                    <div class="col-4"></div>
-                                    <div class="col-4">
-                                        @include('shared._author',[
-                                            'model' => $answer,
-                                            'label' => 'answered'
-                                        ])
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
+                        @include('answers._answer')
                     @endforeach
                 </div>
             </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -18,56 +18,13 @@
                   @include('layouts._messages')
                   
                   @forelse($questions as $question)
-                 		<div class="media">
-                      <div class="d-flex flex-column counters">
-                        <div class="vote">
-                          <strong>{{$question->votes_count}}</strong>{{str_plural('vote', $question->votes_count)}}
-                        </div>
-                        <div class="status {{$question->status}}">
-                          <strong>{{$question->answers_count}}</strong>{{str_plural('answer', $question->answers_count)}}
-                        </div>
-                        <div class="view">
-                          {{$question->views . " " . str_plural('view', $question->views)}}
-                        </div>
-                      </div>
-                 			<div class="media-body">
-                        <div class="d-flex align-items-center">
-                          <h3 class="mt-0">
-                            <a href="{{$question->url}}">{{$question->title}}</a>
-                          </h3>
-                          <div class="ml-auto">
-                            @can('update', $question)
-                            <a href="{{route('questions.edit', $question->id)}}" class="btn btn-outline-info btn-sm">Edit</a>
-                            @endcan
-
-                            @can('delete', $question)
-                            <form action="{{route('questions.destroy', $question->id)}}" method="post" class="form-delete">
-                              @method('DELETE')
-                              @csrf
-                              <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>         
-                            </form>
-                            @endcan
-                          </div>
-                        </div>
-                        <p class="lead">
-                          Asked By
-                          <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                          <small class="text-muted">{{$question->created_date}}</small>
-                        </p>
-                 				<div class="excerpt">
-                         {{ $question->excerpt(400) }}
-                        </div>
-                 			</div>
-                 		</div>
+                 		@include ('questions._excerpt')
                   @empty
                     <div class="alert alert-warning">
                       <strong>Sorry</strong> There are no questions available.
                     </div> 
                   @endforelse
-
-                 <div class="max-auto">
                  		{{$questions->links()}}
-                 </div>
               </div>
             </div>
         </div>
